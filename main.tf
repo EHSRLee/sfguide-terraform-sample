d1350314f5745edfed3243a6685aae3d31eb6f7d
Rich Lee <Richard.Lee@Mass.Gov>
2023-03-15 15:00:59 -0400

    Add Service User, Schema, Grants

--- main.tf
+++ main.tf
@@ -22,20 +22,17 @@ resource "snowflake_warehouse" "warehouse" {
   auto_suspend = 60
 }
 
-provider "snowflake"
-	{
+provider "snowflake" {
         alias = "security_admin"
         role  = "SECURITYADMIN"
 		}
 
-resource "snowflake_role" "role" 
-	{
+resource "snowflake_role" "role" {
 		provider = snowflake.security_admin
 		name     = "TF_DEMO_SVC_ROLE"
 		}
 
-resource "snowflake_database_grant" "grant"
-	{
+resource "snowflake_database_grant" "grant" {
 		provider          = snowflake.security_admin
 		database_name     = snowflake_database.db.name
 		privilege         = "USAGE"
@@ -43,15 +40,13 @@ resource "snowflake_database_grant" "grant"
 		with_grant_option = false
 		}    
 		
-resource "snowflake_schema" "schema" 
-	{
+resource "snowflake_schema" "schema" {
 		database   = snowflake_database.db.name
 		name       = "TF_DEMO"
 		is_managed = false
 		}
 
-resource "snowflake_schema_grant" "grant" 
-	{
+resource "snowflake_schema_grant" "grant" {
 		provider          = snowflake.security_admin
 		database_name     = snowflake_database.db.name
 		schema_name       = snowflake_schema.schema.name
@@ -60,8 +55,7 @@ resource "snowflake_schema_grant" "grant"
 		with_grant_option = false
 		}
 
-resource "snowflake_warehouse_grant" "grant" 
-	{
+resource "snowflake_warehouse_grant" "grant" {
 		provider          = snowflake.security_admin
 		warehouse_name    = snowflake_warehouse.warehouse.name
 		privilege         = "USAGE"
@@ -69,14 +63,12 @@ resource "snowflake_warehouse_grant" "grant"
 		with_grant_option = false
 		}
 
-resource "tls_private_key" "svc_key" 
-	{
+resource "tls_private_key" "svc_key" {
 		algorithm = "RSA"
 		rsa_bits  = 2048
 		}
 
-resource "snowflake_user" "user" 
-	{
+resource "snowflake_user" "user" {
 		provider          = snowflake.security_admin
 		name              = "tf_demo_user"
 		default_warehouse = snowflake_warehouse.warehouse.name
@@ -85,8 +77,7 @@ resource "snowflake_user" "user"
 		rsa_public_key    = substr(tls_private_key.svc_key.public_key_pem, 27, 398)
 		}
 
-resource "snowflake_role_grants" "grants" 
-	{
+resource "snowflake_role_grants" "grants" {
 		provider  = snowflake.security_admin
 		role_name = snowflake_role.role.name
 		users     = [snowflake_user.user.name]
